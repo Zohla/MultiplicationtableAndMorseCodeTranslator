dab2cebb29fd83ef22b66e8327bf2b419fa2bc8a
Maren Lilleberre <marenlilleberre@gmail.com>
2022-10-26 21:20:42 +0200

    fix bug with morse code being written for "q"

--- src/MorseCode.java
+++ src/MorseCode.java
@@ -5,56 +5,65 @@ public class MorseCode {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("What do you want translated to morse code.To exit program, type Q.");
-        String translateThis = "";
-        do {
-            translateThis = scanner.nextLine().toLowerCase();
-                int i = 0;
-                while (i < translateThis.length()) {
-                    Map<Character, String> morse = new HashMap<>();
-                    morse.put('a', ".-");
-                    morse.put('b', "-...");
-                    morse.put('c', "-.-");
-                    morse.put('d', "-..");
-                    morse.put('e', ".");
-                    morse.put('f', "..-.");
-                    morse.put('g', "--.");
-                    morse.put('h', "....");
-                    morse.put('i', "..");
-                    morse.put('j', ".---");
-                    morse.put('k', "-.");
-                    morse.put('l', ".-..");
-                    morse.put('m', "--");
-                    morse.put('n', "-.");
-                    morse.put('o', "---");
-                    morse.put('p', ".--.");
-                    morse.put('q', "--.-");
-                    morse.put('r', ".-.");
-                    morse.put('s', "...");
-                    morse.put('t', "-");
-                    morse.put('u', "..-");
-                    morse.put('v', "...-");
-                    morse.put('w', ".--");
-                    morse.put('x', "-..-");
-                    morse.put('y', "-.--");
-                    morse.put('z', "--..");
-                    morse.put('1', ".----");
-                    morse.put('2', "..---");
-                    morse.put('3', "...--");
-                    morse.put('4', "....-");
-                    morse.put('5', ".....");
-                    morse.put('6', "-....");
-                    morse.put('7', "--...");
-                    morse.put('8', "---..");
-                    morse.put('9', "----.");
-                    morse.put('0', "-----");
-                    morse.put(' ', " || ");//added seperator for spaces
-                    System.out.print(morse.get(translateThis.charAt(i)) + "|"); //Write out all letters and separator between chars
+        System.out.println("What do you want translated to morse code?To exit program, type Q.");
+        boolean keepGoing = true;
 
-                    i++;
-                }
-            System.out.println();
-        }while (!(translateThis.equals("q")));
+        while (keepGoing) {
+            String translateThis = scanner.nextLine().toLowerCase();
+            if (translateThis.equals("q")) {
+                keepGoing = false;
+            } else {
+                translator(translateThis);
+                System.out.println();
+            }
+        }
+    }
+
+    public static void translator(String translate) {
+        int i = 0;
+        while (i < translate.length()) {
+            Map<Character, String> morse = new HashMap<>();
+            morse.put('a', ".-");
+            morse.put('b', "-...");
+            morse.put('c', "-.-");
+            morse.put('d', "-..");
+            morse.put('e', ".");
+            morse.put('f', "..-.");
+            morse.put('g', "--.");
+            morse.put('h', "....");
+            morse.put('i', "..");
+            morse.put('j', ".---");
+            morse.put('k', "-.");
+            morse.put('l', ".-..");
+            morse.put('m', "--");
+            morse.put('n', "-.");
+            morse.put('o', "---");
+            morse.put('p', ".--.");
+            morse.put('q', "--.-");
+            morse.put('r', ".-.");
+            morse.put('s', "...");
+            morse.put('t', "-");
+            morse.put('u', "..-");
+            morse.put('v', "...-");
+            morse.put('w', ".--");
+            morse.put('x', "-..-");
+            morse.put('y', "-.--");
+            morse.put('z', "--..");
+            morse.put('1', ".----");
+            morse.put('2', "..---");
+            morse.put('3', "...--");
+            morse.put('4', "....-");
+            morse.put('5', ".....");
+            morse.put('6', "-....");
+            morse.put('7', "--...");
+            morse.put('8', "---..");
+            morse.put('9', "----.");
+            morse.put('0', "-----");
+            morse.put(' ', " || ");//separator for spaces
+            System.out.print(morse.get(translate.charAt(i)) + "|"); //Write out all letters and a separator between chars for readability
+
+            i++;
+        }
 
     }
 }
